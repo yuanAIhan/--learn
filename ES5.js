54f4463ec9b0cc32c1db98dccc1a0e969114c003
yuanAIhan <1172207034@qq.com>
2021-04-27 14:13:48 +0800

    对象

--- ES5.js
+++ ES5.js
@@ -46,3 +46,56 @@ var square = function (x) {
     // void,instanceof, typeof,
 }
 
+var empty = {};
+var point = {
+    x: 0,
+    y: 0
+};
+var point2 = {
+    x: point.x,
+    y: point.y
+};
+var book = {
+    "main title": "javascript",
+    "sub-title": "The guide",
+    "for": "all audiences",
+    author: {
+        firstName: "David",
+        surName: "Flanagan"
+    }
+};
+
+var o3 = Object.create(Object.prototype);
+
+function inherit(p) {
+    if (p == null) throw TypeError();
+    if (Object.create) {
+        return Object.create(p);
+    }
+    var t = typeof p;
+    if (t !== "object" && t != "function") throw TypeError();
+    function f() { };
+    f.prototype = p;
+    return new f();
+}
+//只是使用对应的继承而来的函数作为一个参数的传递的，可以防止无意间修改其中的原始对象！
+
+function getValue(portfolio) {
+    var total = 0.0;
+    for (stack in portfolio) {
+        var shares = portfolio[stock];
+        var price = getquote(stock);
+        total += shares * price;
+    }
+    return total;
+}
+
+var len = undefined;
+if (book) {
+    if (book["sub-title"]) {
+        len = book["sub-title"].length;
+    }
+}
+var len = book && book["sub-title"] && book["sub-title"].length;
+//需要全部都是真的时候才有最终的结果返回的！所有其是可以ide！
+
